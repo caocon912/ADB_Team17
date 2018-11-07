7a31649bc616e69d16b9cb142621141e4a7e9d01
LAPTOP-133EGMIS\Artic <nguyennhuquynh1698@gmail.com>
2018-11-07 08:14:27 +0700

    Phan1: cau 1 2 3

---
+++ ADBTeam17_DB4O/ADBTeam17_DB4O/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADBTeam17_DB4O
+{
+    class Employee
+    {
+    }
+}
